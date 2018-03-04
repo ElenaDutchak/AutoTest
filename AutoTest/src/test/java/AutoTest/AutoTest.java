425f7dc3c623e4095297c8e0e48d916cdad95763
ElenaDutchak <36824208+ElenaDutchak@users.noreply.github.com>
2018-03-04 19:27:32 +0200

    Add files via upload

---
+++ AutoTest/src/test/java/AutoTest/AutoTest.java
@@ -0,0 +1,48 @@
+package AutoTest;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class AutoTest {
+
+    private static WebDriver driver;
+
+    @BeforeClass
+    public static void setup() {
+        String s = System.setProperty("webdriver.chrome.driver", "c:/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://mail.ukr.net/desktop/login");
+    }
+
+    @Test
+    public void userLogin() {
+        WebElement loginField = driver.findElement(By.id("id-1"));
+        loginField.sendKeys("autotestorgua");
+        WebElement passwordField = driver.findElement(By.id("id-2"));
+        passwordField.sendKeys("testpass");
+        WebElement loginButton = driver.findElement(By.className("button__content"));
+        loginButton.click();
+        WebElement profileUser = driver.findElement(By.cssSelector(".login-button__user"));
+        String mailUser = profileUser.getText();
+        Assert.assertEquals("autotestorgua@ukr.net", mailUser);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        WebElement menuUser = driver.findElement(By.cssSelector(".login-button__menu-icon"));
+        menuUser.click();
+        WebElement logoutButton = driver.findElement(By.id("login__logout"));
+        logoutButton.click();
+        driver.quit();
+    }
+}
